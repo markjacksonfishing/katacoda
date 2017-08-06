2f2a5aa7964e3429519b8f8f1ff0e124f20eff88
Ben Hall <ben@benhall.me.uk>
2017-08-06 21:12:21 +0100

    Breaking uploading of files.

--- sysdig-container-visibility/courseBase.sh
+++ sysdig-container-visibility/courseBase.sh
@@ -1,2 +1,3 @@
 ssh root@host01 "sed -i s/ollie01/[[KATACODA_HOST]]/g /home/ubuntu/wp.sql"
 ssh root@host01 "sed -i s/2886795299/[[HOST_SUBDOMAIN]]/g /home/ubuntu/wp.sql"
+ssh root@host01 "curl -L https://raw.githubusercontent.com/katacoda-scenarios/sysdig-scenarios/master/sysdig-container-visibility/assets/simulate-load.sh -o /home/ubuntu/simulate-load.sh && chmod +x /home/ubuntu/simulate-load.sh"
