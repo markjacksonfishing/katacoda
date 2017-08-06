c137e3b9621f3eba93006d6dfcf94829c8647152
Ben Hall <ben@benhall.me.uk>
2017-08-06 20:41:30 +0100

    File exists on the server

--- sysdig-container-visibility/courseBase.sh
+++ sysdig-container-visibility/courseBase.sh
@@ -1,2 +1,2 @@
-sed -i s/ollie01/[[KATACODA_HOST]]/g wp.sql
-sed -i s/2886795299/[[HOST_SUBDOMAN]]/g wp.sql
+ssh root@host01 "sed -i s/ollie01/[[KATACODA_HOST]]/g /home/ubuntu/wp.sql"
+ssh root@host01 "sed -i s/2886795299/[[HOST_SUBDOMAIN]]/g /home/ubuntu/wp.sql"
