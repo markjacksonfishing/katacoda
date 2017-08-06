641321ac3e0ec3cbf37eec3558b3eb1fff8a5ded
Ben Hall <ben@benhall.me.uk>
2017-08-06 20:55:30 +0100

    Silence curl

--- sysdig-container-visibility/simulate-load.sh
+++ sysdig-container-visibility/simulate-load.sh
@@ -1,243 +1,243 @@
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker -o /dev/null
+curl -s docker -o /dev/null
 sleep 1
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 2
-curl docker -o /dev/null
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 0.6
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
 sleep 3
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 2
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.8
-curl docker/wp-login.php -o /dev/null
+curl -s docker/wp-login.php -o /dev/null
 sleep 0.3
-curl docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
 sleep 3
-curl docker/2017/06/ -o /dev/null
-curl docker/2017/06/21/hello-world/#comment-1 -o /dev/null
-curl docker/2017/06/21/hello-world/ -o /dev/null
+curl -s docker/2017/06/ -o /dev/null
+curl -s docker/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s docker/2017/06/21/hello-world/ -o /dev/null
 sleep 1
