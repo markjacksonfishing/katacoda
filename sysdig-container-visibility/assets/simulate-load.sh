a1b56953db60bf0271c370898b84c369241ab0fd
Ben Hall <ben@benhall.me.uk>
2017-08-06 21:08:33 +0100

    Move simulate-load to a script

---
+++ sysdig-container-visibility/assets/simulate-load.sh
@@ -0,0 +1,243 @@
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost -o /dev/null
+sleep 1
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 2
+curl -s localhost -o /dev/null
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 0.6
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+sleep 3
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
+curl -s localhost/wp-login.php -o /dev/null
+sleep 2
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.8
+curl -s localhost/wp-login.php -o /dev/null
+sleep 0.3
+curl -s localhost/2017/06/ -o /dev/null
+sleep 3
+curl -s localhost/2017/06/ -o /dev/null
+curl -s localhost/2017/06/21/hello-world/#comment-1 -o /dev/null
+curl -s localhost/2017/06/21/hello-world/ -o /dev/null
+sleep 1
