e9664c34aa12a79d43cb7532b4c16b759e2a2efc
John Fitzpatrick <jhnftzptrck~@gmail.com>
2019-11-11 17:37:23 +0000

    Fixt setup script (and added aliases)

--- secure/secure-pod-policy-advisor/courseBase.sh
+++ secure/secure-pod-policy-advisor/courseBase.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/johnfitzpatrick/psp-demo.git
+mkdir -p /audit
+cp psp-demo/audit-policy.yaml /audit
 cp psp-demo/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
-mkdir -p /audit/log
-cp assets/audit-policy.yaml /audit
+alias k='kubectl'
+alias kn='kubectl get nodes'
