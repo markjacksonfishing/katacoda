f7c8c231422ffa6408db22de8238f5d143494542
John Fitzpatrick <jhnftzptrck~@gmail.com>
2019-11-11 16:57:50 +0000

    Cloning repo

--- secure/secure-pod-policy-advisor/courseBase.sh
+++ secure/secure-pod-policy-advisor/courseBase.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/johnfitzpatrick/psp-demo.git
 cp assets/manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 mkdir -p /audit/log
 cp assets/audit-policy.yaml /audit
