c9c9a131267731e707881e4bbc43e1b80c78b290
John Fitzpatrick <jhnftzptrck~@gmail.com>
2019-11-12 18:10:14 +0000

    Resloved conflict

--- secure/secure-forensics-activity-audits/courseBase.sh
+++ secure/secure-forensics-activity-audits/courseBase.sh
@@ -3,5 +3,6 @@ mkdir -p /audit
 mv manifests/audit-policy.yaml /audit
 mv manifests/audit-webhook.yaml /audit
 mv manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+git clone https://github.com/johnfitzpatrick/psp-demo.git
 alias k='kubectl'
 alias kn='kubectl get nodes'
