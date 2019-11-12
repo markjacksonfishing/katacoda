19c91a4b3d5b3f9f2da41b5b3a9153af79f547e1
John Fitzpatrick <jhnftzptrck~@gmail.com>
2019-11-12 21:59:58 +0000

    Troubleshooting API server

--- secure/secure-forensics-activity-audits/courseBase.sh
+++ secure/secure-forensics-activity-audits/courseBase.sh
@@ -2,6 +2,6 @@ mkdir -p manifests
 mkdir -p /audit
 mv manifests/audit-policy.yaml /audit
 mv manifests/audit-webhook.yaml /audit
-mv manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+# mv manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 alias k='kubectl'
 alias kn='kubectl get nodes'
