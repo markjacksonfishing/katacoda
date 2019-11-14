16bfba530fe5f8e20e3be93c447094e61be18058
Jujuyeh <paul.98.zgz@gmail.com>
2019-11-14 16:43:26 +0100

    Fix IP on Activity Audits

--- secure/secure-forensics-activity-audits/courseBase.sh
+++ secure/secure-forensics-activity-audits/courseBase.sh
@@ -4,5 +4,10 @@ mv manifests/audit-policy.yaml /audit
 mv manifests/audit-webhook.yaml /audit
 cp /etc/kubernetes/manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.BAK.yaml
 mv manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+
+# Update the IP
+NEW_IP=$(kubectl config view | grep server | egrep -o "[[:digit:]]+.[[:digit:]]+.[[:digit:]]+.[[:digit:]]+")
+sed -i "s/172.17.0.79/$NEW_IP/" /etc/kubernetes/manifests/kube-apiserver.yaml
+
 alias k='kubectl'
 alias kn='kubectl get nodes'
