dd6f44e604b5989c3342f8d38409ec5194371793
John Fitzpatrick <jhnftzptrck@gmail.com>
2020-01-20 12:49:11 +0000

    Added node_exporter

--- monitor/understanding-promql/courseBase.sh
+++ monitor/understanding-promql/courseBase.sh
@@ -1,6 +1,12 @@
-docker run -d \
-  --net="host" \
-  --pid="host" \
-  -v "/:/host:ro,rslave" \
-  quay.io/prometheus/node-exporter \
-  --path.rootfs=/host
+# # This isn't working
+# docker run -d \
+#   --net="host" \
+#   --pid="host" \
+#   -v "/:/host:ro,rslave" \
+#   quay.io/prometheus/node-exporter \
+#   --path.rootfs=/host
+
+wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.linux-amd64.tar.gz
+tar -xzf node_exporter-*.linux-amd64.tar.gz
+cd node_exporter-*.linux-amd64
+./node_exporter &
