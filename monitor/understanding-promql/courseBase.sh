c275b37fc1ddcfc313a8a82ffd2253b8da46adf9
John Fitzpatrick <jhnftzptrck@gmail.com>
2020-01-20 13:00:23 +0000

    Added launch_node_exporter.sh to coursebase.sh

--- monitor/understanding-promql/courseBase.sh
+++ monitor/understanding-promql/courseBase.sh
@@ -6,7 +6,9 @@
 #   quay.io/prometheus/node-exporter \
 #   --path.rootfs=/host
 
-wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.linux-amd64.tar.gz
-tar -xzf node_exporter-*.linux-amd64.tar.gz
-cd node_exporter-*.linux-amd64
-./node_exporter &
+# wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.linux-amd64.tar.gz
+# tar -xzf node_exporter-*.linux-amd64.tar.gz
+# cd node_exporter-*.linux-amd64
+# ./node_exporter &
+
+./launch_node_exporter.sh
